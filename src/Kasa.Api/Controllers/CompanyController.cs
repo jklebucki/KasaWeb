b14f82558f4c6969f6032fa6b98a283624426dae
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2022-04-11 22:03:43 +0200

    Company controller

---
+++ src/Kasa.Api/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using Kasa.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kasa.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyController: ControllerBase
+    {
+        private readonly ICompanyService _companyservice;
+
+        public CompanyController(ICompanyService companyservice)
+        {
+            _companyservice = companyservice;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompany(int id)
+        {
+            var company = await _companyservice.GetCompanyById(id);
+            return Ok(company);
+        } 
+    }
+}
