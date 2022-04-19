a07c5a53ce3154f325ab46e252a3166ab5b03b72
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2022-04-19 22:59:36 +0200

    Location bank account model

--- src/Kasa.Core/Domain/LocationBankAccount.cs
+++ src/Kasa.Core/Domain/LocationBankAccount.cs
@@ -5,5 +5,40 @@ namespace Kasa.Core.Domain
         public int LocationId { get; set; }
         public string BankName { get; set; }
         public string BankAccount { get; set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime? UpdatedAt { get; protected set; }
+
+        private LocationBankAccount() { }
+
+        public LocationBankAccount(int locationId, string bankName, string bankAccount)
+        {
+            SetLocationId(locationId);
+            SetName(bankName);
+            SetBankAccount(bankAccount);
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        private void SetLocationId(int locationId)
+        {
+            LocationId = locationId;
+        }
+
+        private void SetName(string bankName)
+        {
+            BankName = bankName;
+        }
+
+        private void SetBankAccount(string bankAccount)
+        {
+            BankAccount = bankAccount;
+        }
+
+        public void Update(int locationId, string bankName, string bankAccount)
+        {
+            SetLocationId(locationId);
+            SetName(bankName);
+            SetBankAccount(bankAccount);
+            UpdatedAt = DateTime.UtcNow;
+        }
     }
 }
